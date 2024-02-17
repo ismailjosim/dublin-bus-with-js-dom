4654c8a4cd2b75a84e1bc1ef364a4ebe7529d83d
ismailjosim <ismailjosim99@gmail.com>
2024-02-17 16:19:26 +0600

    select your seat design part completed

--- JS/app.js
+++ JS/app.js
@@ -2,13 +2,16 @@
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenuEl = document.getElementById("mobileMenu");
 
-menuBtn.addEventListener('click', handleToggleMenu = () => {
+// Menu icons
+menuBtn.addEventListener('click', function () {
     menuBtn.children[0].classList.toggle("hidden")
     const menuCloseBtn = document.getElementById("close-icon");
     menuCloseBtn.classList.toggle("hidden")
     mobileMenuEl.classList.toggle("hidden")
     mobileMenuEl.classList.toggle("flex")
 })
+
+
 
 function handleSelectSeat(event) {
     const selectedSeat = event.innerText
