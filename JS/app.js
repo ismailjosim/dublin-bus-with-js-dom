eb7877e9342b4dddc6802d0153ae0234a076389e
ismailjosim <ismailjosim99@gmail.com>
2024-02-17 17:14:07 +0600

    seat selection, price increase, seat decrease added

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,9 @@
 
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenuEl = document.getElementById("mobileMenu");
+const seatSelectedEl = document.getElementById("selected-seat")
+const totalPriceEl = document.getElementById("total-price")
+const availableSeatEl = document.getElementById("available-seat")
 
 // Menu icons
 menuBtn.addEventListener('click', function () {
@@ -11,9 +14,40 @@ menuBtn.addEventListener('click', function () {
     mobileMenuEl.classList.toggle("flex")
 })
 
-
+let selectedSeat = []
+let totalPrice = 0;
 
 function handleSelectSeat(event) {
-    const selectedSeat = event.innerText
-    console.log(selectedSeat)
+    const value = event.innerText
+    if (selectedSeat.includes(value)) {
+        return alert("Seat already added")
+    } else {
+        if (selectedSeat.length < 4) {
+            event.classList.add("bg-primary")
+            event.classList.add("text-white")
+
+            selectedSeat.push(value);
+            totalPrice += 550;
+
+            totalPriceEl.innerText = totalPrice;
+
+            // decrease available seat
+            const availableSeatValue = parseFloat(availableSeatEl.innerText);
+            const newAvailableSeatValue = availableSeatValue - 1;
+            availableSeatEl.innerText = newAvailableSeatValue
+
+            seatSelectedEl.innerHTML += `
+         <li class="text-base font-normal flex justify-between">
+            <span>${ value }</span>
+            <span>Economy</span>
+            <span>550</span>
+        </li>
+        `
+        } else {
+            return alert("Maximum seat added")
+        }
+
+
+    }
 }
+
